e226c1870b863e38cc68c37187a56a23e074b8ec
LucasJSM <lucasjsm2001@gmail.com>
2022-07-07 17:47:08 -0300

    update variable names

--- desafio3.ts
+++ desafio3.ts
@@ -1,40 +1,40 @@
-let botaoAtualizar = document.getElementById("atualizar-saldo")!;
-let botaoLimpar = document.getElementById("limpar-saldo")!;
+let updateButton = document.getElementById("atualizar-saldo")!;
+let clearButton = document.getElementById("limpar-saldo")!;
 
-let soma = document.getElementById("soma")! as HTMLInputElement;
-let campoSaldo = document.getElementById("campo-saldo")!;
+let sum = document.getElementById("soma")! as HTMLInputElement;
+let balanceField = document.getElementById("campo-saldo")!;
 
-let ultimoValor = document.getElementById("ultimo-valor")!;
+let lastValue = document.getElementById("ultimo-valor")!;
 
 let total = 0;
 
-const limparCampoSoma = (): void => {
-  soma.value = "";
-  soma.focus();
+const clearSumField = (): void => {
+  sum.value = "";
+  sum.focus();
 };
 
-limparCampoSoma();
+clearSumField();
 
-const somarAoSaldo = (soma: number): void => {
-  total += soma;
-  ultimoValor.innerHTML = soma.toString();
-  campoSaldo.innerHTML = total.toString();
-  limparCampoSoma();
+const addToBalance = (sum: number): void => {
+  total += sum;
+  lastValue.innerHTML = sum.toString();
+  balanceField.innerHTML = total.toString();
+  clearSumField();
 };
 
-const limparSaldo = (): void => {
+const clearBalance = (): void => {
   total = 0;
-  ultimoValor.innerHTML = total.toString();
-  campoSaldo.innerHTML = total.toString();
-  limparCampoSoma();
+  lastValue.innerHTML = total.toString();
+  balanceField.innerHTML = total.toString();
+  clearSumField();
 };
 
-botaoAtualizar.addEventListener("click", () => {
-  somarAoSaldo(Number(soma.value));
-  limparCampoSoma();
+updateButton.addEventListener("click", () => {
+  addToBalance(Number(sum.value));
+  clearSumField();
 });
 
-botaoLimpar.addEventListener("click", () => {
-  limparSaldo();
-  limparCampoSoma();
+clearButton.addEventListener("click", () => {
+  clearBalance();
+  clearSumField();
 });
